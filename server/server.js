667c3483b83e1cd804212c45e70f1ce6086cd5a4
mohit-singh-jat-git <168964255+mohit-singh-jat-git@users.noreply.github.com>
2025-02-16 01:40:15 +0530

    npm add

--- server/server.js
+++ server/server.js
@@ -1,10 +1,5 @@
 const express = require("express");
 const app = express();
-app.listen(8082,()=>{
-    console.log("server is running ")
-})
-
-
-
-
-
+app.listen(8082, () => {
+  console.log("server is running ");
+});
